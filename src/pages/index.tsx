7f00970453253cd403242f8e14865e13ac110c83
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2022-11-11 11:14:22 -0300

    'inicio'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Builder Seu Negócio Online</title>
         <meta name="Builder Seu Negócio Online" content="Desenvolva sua solução online para sua empresa e negócio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
-    </div>
+      <h1>Site</h1>
+
+    </>
   )
 }
