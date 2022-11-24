cef3a8247d19bab3148abd6b2246d4945d294655
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-11-23 22:34:53 -0300

    'finish'

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteUrl: 'https://builderseunegocioonline.com.br/',
+    generateRobotsTxt: true, // opcional
+    priority: null,
+    changefreq: null,
+    exclude: ['/server-sitemap.xml', '/post/*'],
+    robotsTxtOptions: {
+      additionalSitemaps: ['https://builderseunegocioonline.com.br/server-sitemap.xml'],
+    },
+  }
