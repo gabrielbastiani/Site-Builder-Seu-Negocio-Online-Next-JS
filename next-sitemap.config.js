2464b5e31a8f7cb5ab68c320aa13460acbb0e1ff
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2022-11-24 11:07:36 -0300

    'deploy'

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://builderseunegocioonline.com.br',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
